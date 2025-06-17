31db20cdf30b308fbe603f85e56c68b5140b93bc
Krapaud <mur.mickael@gmail.com>
2025-06-17 13:19:43 +0200

    Add 1-last_digit.c to determine the last digit of a random number

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Assign a Random number variable every time
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int last_digit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last_digit = n % 10;
+		printf("Last digit of %d is", last_digit);
+	if (last_digit > 5)
+	{
+		printf("and is greater than 5\n");
+	}
+	else if (last_digit == 0)
+	{
+		printf("and is 0\n");
+	}
+	else if (last_digit < 6 && n != 0)
+	{
+		printf("and is less than 6 and not 0\n");
+	}
+	return (0);
+}

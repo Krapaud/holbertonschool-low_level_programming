6825a693bb5469c15c5b3fa7d3f1b679041a0971
Krapaud <mur.mickael@gmail.com>
2025-06-17 11:18:42 +0200

    Refactor comments and formatting in 0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,32 +1,35 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
 /**
-* main - Entry point
-* Assign random number
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Description: Assigns a random number to variable n and prints whether
+ * the number is positive, zero, or negative.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-int n;
+	int n;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-printf("%d ", n);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("%d ", n);
 
-if (n > 0)
-{
-printf("is positive\n");
-}
-else if (n == 0)
-{
-printf("is zero\n");
-}
-else
-{
-printf("is negative\n");
-}
-return (0);
+	if (n > 0)
+	{
+		printf("is positive\n");
+	}
+	else if (n == 0)
+	{
+		printf("is zero\n");
+	}
+	else
+	{
+		printf("is negative\n");
+	}
+
+	return (0);
 }

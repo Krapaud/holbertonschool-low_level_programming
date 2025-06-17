186229cc305271f9afd78e4d085eeebf8bd7ff96
Krapaud <mur.mickael@gmail.com>
2025-06-17 15:09:46 +0200

    Add 9-print_comb.c to print all single digit numbers in base 10

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print all single digit number base 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = '0'; number <= '9'; number++)
+	{
+		putchar(number);
+		if (number != '9')
+		{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}

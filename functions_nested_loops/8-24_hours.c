89f293fe9fcd9a307245ca6bcd48b2d2d45f8e52
Krapaud <mur.mickael@gmail.com>
2025-06-19 14:47:31 +0200

    Refactor jack_bauer function to use _putchar for output and correct loop boundaries

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+#include <unistd.h>
 /**
  * jack_bauer - Prints every minute of the day of Jack Bauer,
  * from 00:00 to 23:59.
@@ -9,11 +9,17 @@ void jack_bauer(void)
 {
 	int hour, minute;
 
-	for (hour = 0; hour < 24; hour++)
+	for (hour = 0; hour < 23; hour++)
 	{
-		for (minute = 0; minute < 60; minute++)
+		for (minute = 0; minute <= 59; minute++)
 		{
-			printf("%02d:%02d\n", hour, minute);
-		}
-	}
+			   _putchar((hour / 10) + '0');
+   _putchar((hour % 10) + '0');
+   _putchar(':');
+   _putchar((minute / 10) + '0');
+   _putchar((minute % 10) + '0');
+   _putchar('\n');
+  }
+   }
 }
+

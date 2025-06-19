10fc0286f941d3fd0cbba916bd95256600c2e2d7
Krapaud <mur.mickael@gmail.com>
2025-06-19 19:57:07 +0200

    Refactor add function to simplify implementation and improve documentation

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -2,21 +2,12 @@
 
 /**
  * add - Adds two integers and returns the result
- * @a: first integer
- * @b: second integer
+ * @a: First integer
+ * @b: Second integer
  *
- * Return: sum of a and b
+ * Return: The sum of a and b
  */
 int add(int a, int b)
 {
-	int sum = a + b;
-
-	if (sum >= 10)
-	{
-	_putchar((sum / 10) + '0');
-	}
-	_putchar((sum % 10) + '0');
-	_putchar('\n');
-
-	return (sum);
+	return (a + b);
 }

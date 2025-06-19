c2cf72768d0fdd31774892c93efdf9b1369fc002
Krapaud <mur.mickael@gmail.com>
2025-06-19 18:30:34 +0200

    Implement times_table function to print the 9 times table with proper formatting

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -5,17 +5,34 @@
  *
  * Return: void
  */
-
 void times_table(void)
 {
-	int a, b;
-	for (a = 0; a < 10; a++)
+	int a, b, result;
 
+	for (a = 0; a <= 9; a++)
 	{
-		for (b = 0; b < 10; b++)
+		for (b = 0; b <= 9; b++)
 		{
-		_putchar()
+			result = a * b;
+
+			if (b != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (result < 10)
+				_putchar(' ');
+			}
+
+			if (result >= 10)
+			{
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+			else
+			{
+				_putchar(result + '0');
+			}
 		}
-		_putchar("\n");
+		_putchar('\n');
 	}
 }

78608efa2eb63261b95e9cc1790b176a16b5b21f
Krapaud <mur.mickael@gmail.com>
2025-06-22 15:58:43 +0200

    Add configuration files for code formatting and task automation; remove obsolete settings

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,53 +1,32 @@
-#include "main.h"
 #include <stdio.h>
-
+#include "main.h"
 /**
- * print_times_table - Prints the n times table, starting with 0.
- * @n: number of the times table
+ * times_table - Prints the 9 times table, starting with 0.
+ *
  * Return: void
  */
-void print_times_table(int n)
+void times_table (void)
 {
-	int a, b, result;
+    int a, b, result;
 
-	if (n < 0 || n > 15)
-		return;
+    for (a = 0; a <= 12; a++) {
+        for (b = 0; b <= 12; b++) {
+            result = a * b;
 
-	for (a = 0; a <= n; a++)
-	{
-		for (b = 0; b <= n; b++)
-		{
-			result = a * b;
+            if (b != 0) {
+                _putchar (',');
+                _putchar (' ');
+                if (result < 13)
+                    _putchar (' ');
+            }
 
-			if (b == 0)
-			{
-				_putchar(result + '0');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-
-				if (result < 10)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(result + '0');
-				}
-				else if (result < 100)
-				{
-					_putchar(' ');
-					_putchar((result / 10) + '0');
-					_putchar((result % 10) + '0');
-				}
-				else
-				{
-					_putchar((result / 100) + '0');
-					_putchar(((result / 10) % 10) + '0');
-					_putchar((result % 10) + '0');
-				}
-			}
-		}
-		_putchar('\n');
-	}
+            if (result >= 10) {
+                _putchar ((result / 10) + '0');
+                _putchar ((result % 10) + '0');
+            } else {
+                _putchar (result + '0');
+            }
+        }
+        _putchar ('\n');
+    }
 }

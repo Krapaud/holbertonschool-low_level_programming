cb27029b3fb13107ff460b92db99055dda6a6c59
Krapaud <mur.mickael@gmail.com>
2025-06-22 16:30:49 +0200

    Refactor print_times_table function for clarity and efficiency by renaming variables and simplifying conditions

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,14 +1,12 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * print_times_table - Prints the n times table, starting with 0.
  * @n: The number of the times table to print (0 <= n <= 15)
- *
- * Return: void
  */
 void print_times_table(int n)
 {
-	int a, b, result;
+	int a, b, r;
 
 	if (n < 0 || n > 15)
 		return;
@@ -17,38 +15,26 @@ void print_times_table(int n)
 	{
 		for (b = 0; b <= n; b++)
 		{
-			result = a * b;
-
-			if (b != 0)
+			r = a * b;
+			if (b)
 			{
 				_putchar(',');
 				_putchar(' ');
-				if (result < 10)
-				{
+				if (r < 10)
+					_putchar(' '), _putchar(' ');
+				else if (r < 100)
 					_putchar(' ');
-					_putchar(' ');
-				}
-				else if (result < 100)
-				{
-					_putchar(' ');
-				}
 			}
-
-			if (result >= 100)
-			{
-				_putchar((result / 100) + '0');
-				_putchar(((result / 10) % 10) + '0');
-				_putchar((result % 10) + '0');
-			}
-			else if (result >= 10)
-			{
-				_putchar((result / 10) + '0');
-				_putchar((result % 10) + '0');
-			}
+			if (r >= 100)
+				_putchar((r / 100) + '0');
+			if (r >= 10)
+				_putchar(((r / 10) % 10) + '0');
+			else if (r >= 100)
+				_putchar('0');
+			if (r >= 10)
+				_putchar((r % 10) + '0');
 			else
-			{
-				_putchar(result + '0');
-			}
+				_putchar(r + '0');
 		}
 		_putchar('\n');
 	}

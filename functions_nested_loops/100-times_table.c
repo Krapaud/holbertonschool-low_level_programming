831784792c8bdda4ea3adf7de25f537bebde14ba
Krapaud <mur.mickael@gmail.com>
2025-06-22 16:28:12 +0200

    Refactor print_times_table function for improved functionality and add new implementation

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,32 +1,55 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 /**
- * times_table - Prints the 9 times table, starting with 0.
+ * print_times_table - Prints the n times table, starting with 0.
+ * @n: The number of the times table to print (0 <= n <= 15)
  *
  * Return: void
  */
-void times_table (void)
+void print_times_table(int n)
 {
-    int a, b, result;
+	int a, b, result;
 
-    for (a = 0; a <= 12; a++) {
-        for (b = 0; b <= 12; b++) {
-            result = a * b;
+	if (n < 0 || n > 15)
+		return;
 
-            if (b != 0) {
-                _putchar (',');
-                _putchar (' ');
-                if (result < 13)
-                    _putchar (' ');
-            }
+	for (a = 0; a <= n; a++)
+	{
+		for (b = 0; b <= n; b++)
+		{
+			result = a * b;
 
-            if (result >= 10) {
-                _putchar ((result / 10) + '0');
-                _putchar ((result % 10) + '0');
-            } else {
-                _putchar (result + '0');
-            }
-        }
-        _putchar ('\n');
-    }
+			if (b != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (result < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else if (result < 100)
+				{
+					_putchar(' ');
+				}
+			}
+
+			if (result >= 100)
+			{
+				_putchar((result / 100) + '0');
+				_putchar(((result / 10) % 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+			else if (result >= 10)
+			{
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+			else
+			{
+				_putchar(result + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
---
+++ functions_nested_loops/print_times_table.c
@@ -0,0 +1,7 @@
+#include "main.h"
+#include <stdio.h>
+
+void print_times_table(int n)
+{
+    // Code pour afficher la table de multiplication de n
+}

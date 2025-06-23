d4fe4d77dba8e69b87d792d3b12d5dee0a895451
Krapaud <mur.mickael@gmail.com>
2025-06-23 14:11:42 +0200

    Refactor print_diagonal function to improve readability and remove unnecessary code

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -9,12 +9,20 @@
 
 void print_diagonal(int n)
 {
-	{
-	if (n <= 0)
-	_putchar('\n');
-	}
+	int i, j;
 
-	while (n > 0)
-	_putchar('_');
-	_putchar('\n');
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
 }

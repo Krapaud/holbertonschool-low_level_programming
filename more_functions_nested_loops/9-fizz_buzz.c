84d918a5ae4760436e8343d5332dbac1330a81e6
Krapaud <mur.mickael@gmail.com>
2025-06-23 15:18:39 +0200

    Add documentation comment for main function in fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * main - prints the numbers from 1 to 100, but for multiples of three
+ *        prints "Fizz" instead of the number and for the multiples of five
+ *        prints "Buzz". For numbers which are multiples of both three and five,
+ *        prints "FizzBuzz".
+ *
+ * Return: Always 0.
+ */
 int
 main (void) {
     int i;

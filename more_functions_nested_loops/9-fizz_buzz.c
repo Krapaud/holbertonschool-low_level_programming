e6871db19d1b9c31b1f3cbc5b91cd54518bcba84
Krapaud <mur.mickael@gmail.com>
2025-06-23 15:35:30 +0200

    Refactor formatting in FizzBuzz function for consistency

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,21 +10,21 @@
  */
 int
 main (void) {
-    int i;
+	int i;
 
-    for (i = 1; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-            printf ("FizzBuzz");
-        else if (i % 3 == 0)
-            printf ("Fizz");
-        else if (i % 5 == 0)
-            printf ("Buzz");
-        else
-            printf ("%d", i);
+			printf ("FizzBuzz");
+		else if (i % 3 == 0)
+			printf ("Fizz");
+		else if (i % 5 == 0)
+			printf ("Buzz");
+		else
+			printf ("%d", i);
 			if (i != 100)
-    	putchar (' ');
+		putchar (' ');
 	}
 	putchar('\n');
-    return (0);
+	return (0);
 }

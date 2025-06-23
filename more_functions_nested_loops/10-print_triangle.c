9fb839fd2e078efddf6c49f1a3fb64de99cc0ed0
Krapaud <mur.mickael@gmail.com>
2025-06-23 16:06:03 +0200

    Add print_triangle function and update header file

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+void
+print_triangle (int size)
+
+{
+	int i, j, k;
+
+	if (size <= 0) {
+		_putchar ('\n');
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+			_putchar(' ');
+		for (k = 0; k < i + 1; k++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}

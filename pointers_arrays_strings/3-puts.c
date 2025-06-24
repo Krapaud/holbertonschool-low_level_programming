2ef35d225395f4d70cc93c4cb0c17f12f7c8e73d
Krapaud <mur.mickael@gmail.com>
2025-06-24 15:29:04 +0200

    Add print_rev function and update main.h header Fix formatting in _puts function

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,11 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * _puts - prints a string to stdout
+ * @str: the string to print
+ *
+ * Return: void
+ */
+
 void _puts(char *str)
 {
 	int len = 0;
 
-	while(str[len] != '\0')
+	while (str[len] != '\0')
 	{
 		_putchar(str[len]);
 		len++;

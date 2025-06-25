1adea8207721b80a1e30defe9d6e1767a1d2e8d7
Krapaud <mur.mickael@gmail.com>
2025-06-25 19:34:57 +0200

    Add documentation for _strncpy function

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,14 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * _strncpy - Copies up to n characters from the string pointed to by src
+ *            to the buffer pointed to by dest.
+ * @dest: The destination buffer.
+ * @src: The source string.
+ * @n: The maximum number of characters to copy.
+ *
+ * Return: A pointer to the destination buffer dest.
+ */
 
 char *_strncpy(char *dest, char *src, int n)
 {

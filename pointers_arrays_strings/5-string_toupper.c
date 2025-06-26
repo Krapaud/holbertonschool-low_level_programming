0c213b86e1bb84c6fe3ed75f27a32fd096d83614
Krapaud <mur.mickael@gmail.com>
2025-06-26 12:18:49 +0200

    Fix formatting and improve documentation in string_toupper function

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,22 +1,20 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * string_toupper - changes all lowercase letters of a string to uppercase
- * @str: the string to be modified
+ * string_toupper - Converts all lowercase letters in a string to uppercase.
+ * @str: The string to be converted.
  *
- * Return: the modified string
+ * Return: A pointer to the converted string.
  */
 
-char *string_toupper(char *str)
+char * string_toupper (char *str)
 {
-	int i = 0;
+    int i = 0;
 
-	while (str[i] != '\0')
-	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-			str[i] = str[i] - 32;
-		i++;
-	}
-	return (str);
+    while (str[i] != '\0') {
+        if (str[i] >= 'a' && str[i] <= 'z')
+            str[i] = str[i] - 32;
+        i++;
+    }
+    return (str);
 }

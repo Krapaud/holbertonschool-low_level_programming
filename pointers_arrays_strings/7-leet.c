1bf992be03915af847310c2b8b14c03c89169c54
Krapaud <mur.mickael@gmail.com>
2025-06-27 15:04:25 +0200

    Implement leet function to encode a string into 1337 speak

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,10 +1,22 @@
 #include "main.h"
 #include <stdio.h>
 
-char *leet(char * s)
+char *leet(char *s)
 {
-	int i;
+	int i = 0;
+	int j = 0;
+	char alph[] = "aAeEoOtTlL";
+	char replace[] = "4433007711";
 
-	for (i = 0; s[i] != '\0'; i++)
-
+	while (s[i] != '\0')
+	{
+		j = 0;
+		for (; alph[j] != '\0'; j++)
+		{
+			if (s[i] == alph[j])
+				s[i] = replace[j];
+		}
+		i++;
+	}
+	return (s);
 }

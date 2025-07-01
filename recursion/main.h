558251b78b21263618cd12a6ab1d4021ef9c840b
Krapaud <mur.mickael@gmail.com>
2025-07-01 14:40:08 +0200

    Add _strlen_recursion function to calculate string length recursively

--- recursion/main.h
+++ recursion/main.h
@@ -54,5 +54,8 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _strlen_recursion(char *s);
 
 #endif

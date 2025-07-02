91ac4a5ef72db8db13e4a182c481480ec3a3e761
Krapaud <mur.mickael@gmail.com>
2025-07-02 09:22:29 +0200

    Add docstring for factorial function to improve code documentation

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,5 +1,11 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * factorial - returns the factorial of a number
+ * @n: the number to calculate the factorial of
+ *
+ * Return: the factorial of n, or -1 if n is negative
+ */
 
 int factorial(int n)
 {

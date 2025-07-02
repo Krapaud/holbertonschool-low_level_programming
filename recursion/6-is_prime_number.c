25f7e9a6c3fd018188e6699167e594b5e62ff973
Krapaud <mur.mickael@gmail.com>
2025-07-02 13:55:58 +0200

    Fix documentation and formatting in is_prime_number function

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -3,7 +3,7 @@
 /**
  * is_prime_number - Checks if a number is prime
  * @n: The number to check
- *
+ * @d: The current divisor
  * Return: 1 if n is prime, 0 otherwise
  */
 int is_prime_rec(int n, int d);
@@ -12,7 +12,7 @@ int is_prime_number(int n)
 {
 	if (n < 2)
 		return (0);
-	
+
 	return (is_prime_rec(n, 2));
 }
 /**
@@ -26,9 +26,9 @@ int is_prime_rec(int n, int d)
 {
 	if (d * d > n)
 		return (1);
-	
+
 	if (n % d == 0)
 		return (0);
-	
+
 	return (is_prime_rec(n, d + 1));
 }

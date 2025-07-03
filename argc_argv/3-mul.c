91fb82f887583e229e5a8ad78130504cbcfbf863
Krapaud <mur.mickael@gmail.com>
2025-07-03 13:18:10 +0200

    Fix argument types in main function and implement multiplication logic

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+int main(int argc, int *argv[])
 {
-	
+	int i;
+	int j;
+	int result;
+
+	if (argc != 3)
+	{
+		printf("Error'\n");
+	return (1);
+	}
+	i = argv[1];
+	j = argv[2];
+	result = i * j;
+
+	printf('%d\n', result);
+	return(0);
 }

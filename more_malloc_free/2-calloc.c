d785d448e524633f4842d359657b4c923d6d6858
Krapaud <mur.mickael@gmail.com>
2025-07-08 15:14:02 +0200

    Fix _calloc to allocate the correct number of bytes and initialize array elements

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -13,11 +13,14 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int *arr;
 	unsigned int i;
 
-	arr = malloc(nmemb * sizeof(int));
+	if (nmemb == 0 || size == 0)
+	return (NULL);
+
+	arr = malloc(nmemb * (size + 1));
 	if (arr == NULL)
 		return (NULL);
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < nmemb; i++)
 	{
 		arr[i] = 0;
 	}

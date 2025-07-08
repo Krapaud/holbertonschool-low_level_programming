36920066ff07dbdbb86fa32f4f2bee81634b2197
Krapaud <mur.mickael@gmail.com>
2025-07-08 13:45:12 +0200

    Fix malloc_checked function to allocate the correct amount of memory

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,15 @@
 #include "main.h"
 #include "stdlib.h"
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: the number of bytes to allocate
+ * Return: a pointer to the allocated memory
+ */
 
 void *malloc_checked(unsigned int b)
 {
-int *i;
+	void *i = malloc(sizeof(b));
 
-	i = malloc(sizeof(b));
 	if (i == NULL)
 	{
 		exit(98);

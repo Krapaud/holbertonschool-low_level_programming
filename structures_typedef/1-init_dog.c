fd82a0294fa3d92477b2d9b1410cbad70e3ccad2
Krapaud <mur.mickael@gmail.com>
2025-07-09 10:07:04 +0200

    Add init_dog function to initialize dog structures and update dog.h

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * init_dog - Initializes a dog structure
+ * @d: Pointer to the dog structure to initialize
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->owner = owner;
+	d->age = age;
+}

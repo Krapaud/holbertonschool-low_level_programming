d62b3e9f21f634b6fcf02c0ea73c58c02bd0f694
Krapaud <mur.mickael@gmail.com>
2025-07-09 14:18:54 +0200

    Fix print_dog function to improve output formatting for NULL pointers

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -15,18 +15,23 @@ void print_dog(struct dog *d)
 	return;
 
 	if (d->name == NULL)
-		printf("(nil)\n");
-
-	if (d->owner == NULL)
-		printf("(nil)\n");
-
-	if (isnan(d->age))
-		printf("(nil)\n");
-
+		printf("Name: (nil)\n");
 	else
 	{
 		printf("Name: %s\n", d->name);
+	}
+
+	if (isnan(d->age))
+		printf("Age: (nil)\n");
+	else
+	{
 		printf("Age: %f\n", d->age);
+	}
+
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+	{
 		printf("Owner: %s\n", d->owner);
 	}
 }

84946754854f4768d0e20d84f6fa09e4c6662192
Krapaud <mur.mickael@gmail.com>
2025-07-09 14:04:45 +0200

    Fix print_dog function to correctly handle NULL pointers and NaN values

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -14,11 +14,13 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 	return;
 
-	if (d->name == NULL || d->owner == NULL || d->age == NAN)
-	printf("(nil)\n");
+	if (d->name == NULL && d->owner == NULL)
+		printf("(nil)\n");
 
 	else
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	{
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
+	}
 }

89414b5be4750f27feffd4ef76389e3e4e271adb
Krapaud <mur.mickael@gmail.com>
2025-07-09 15:57:52 +0200

    Add new_dog function to create a new dog structure

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,46 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * new_dog - Creates a new dog structure
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Return: Pointer to the newly created dog structure, or NULL if it fails.
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dt;
+	char *name_copy, *owner_copy;
+
+	dt = malloc(sizeof(dog_t));
+	if (dt == NULL)
+	{
+		return (NULL);
+	}
+	name_copy = malloc(strlen(name) + 1);
+	if (name_copy == NULL)
+	{
+		free(dt);
+		return (NULL);
+	}
+	strcpy(name_copy, name);
+
+	owner_copy = malloc(strlen(owner) + 1);
+	if (owner_copy == NULL)
+	{
+		free(name_copy);
+		free(dt);
+		return (NULL);
+	}
+	strcpy(owner_copy, owner);
+
+	dt->name = name_copy;
+	dt->age = age;
+	dt->owner = owner_copy;
+
+	return (dt);
+}

dec8d7b310241240957caa22a8e11100c7a20a7c
Krapaud <mur.mickael@gmail.com>
2025-07-09 09:39:06 +0200

    Add dog structure definition in dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - Structure that defines a new type for a dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

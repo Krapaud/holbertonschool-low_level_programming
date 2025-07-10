ebf91f8ff2c37a2cd9bdeaed0a644efd88383231
Krapaud <mur.mickael@gmail.com>
2025-07-10 11:50:42 +0200

    Refactor new_dog function to improve memory allocation and handling of NULL pointers

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -8,42 +8,48 @@
  * @age: Age of the dog
  * @owner: Owner of the dog
  *
- * Return: Pointer to the newly created dog structure, or NULL if it fails.
+ * Return: Pointer to the newly created dog structure,
+ *         or NULL if memory allocation fails or if input pointers are NULL.
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dt;
-	char *name_copy, *owner_copy;
+	int i;
+	int name_len;
+	int owner_len;
 
 	if (name == NULL || owner == NULL)
-	return (NULL);
+		return (NULL);
+
+	name_len = strlen(name);
+	owner_len = strlen(owner);
 
 	dt = malloc(sizeof(dog_t));
 	if (dt == NULL)
 	{
 		return (NULL);
 	}
-	name_copy = malloc(strlen(name) + 1);
-	if (name_copy == NULL)
+	dt->name = malloc(name_len + 1);
+	if (dt->name == NULL)
 	{
 		free(dt);
 		return (NULL);
 	}
-	strcpy(name_copy, name);
 
-	owner_copy = malloc(strlen(owner) + 1);
-	if (owner == NULL)
+	for (i = 0; i < name_len; i++)
+		dt->name[i] = name[i];
+
+	dt->owner = malloc(owner_len + 1);
+	if (dt->owner == NULL)
 	{
-		free(name_copy);
+		free(dt->name);
 		free(dt);
 		return (NULL);
 	}
-	strcpy(owner_copy, owner);
+	for (i = 0; i < owner_len; i++)
+		dt->owner[i] = owner[i];
 
-	dt->name = name_copy;
 	dt->age = age;
-	dt->owner = owner_copy;
-
 	return (dt);
 }

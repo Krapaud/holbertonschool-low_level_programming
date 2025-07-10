d20ed0477f7b7fb5c5b3b20e35cee4db1735b41a
Krapaud <mur.mickael@gmail.com>
2025-07-10 12:11:12 +0200

    Implement free_dog function to free memory allocated for dog_t struct

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - Frees memory allocated for a dog_t struct and its members.
+ * @d: Pointer to the dog_t struct to free.
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d);
+	return (NULL);
+}

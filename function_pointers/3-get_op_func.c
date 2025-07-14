32060f33588f970d207f8bd126c2842977b157c3
Krapaud <mur.mickael@gmail.com>
2025-07-14 10:31:28 +0200

    Implement get_op_func to select operation function based on operator

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,21 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * get_op_func - Selects the correct operation function based on the operator.
+ * @s: The operator passed as a string.
+ *
+ * This function returns a pointer to the appropriate operation function
+ * based on the operator provided. If the operator is not recognized,
+ * it returns NULL.
+ *
+ * Return: Pointer to the function that corresponds to the operator,
+ *         or NULL if the operator is not found.
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	int i = 0;
+
+	while (!= '\0')
+}

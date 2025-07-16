0ec373509db0dc638db14ebcc0c9074327296f7c
Krapaud <mur.mickael@gmail.com>
2025-07-16 12:23:39 +0200

    Add print_all function and related print functions for variadic argument handling

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,92 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_all - prints anything
+ * @format: a list of types of arguments passed to the function
+ *
+ * Return: nothing
+ */
+
+void print_char(va_list args)
+{
+	 printf("%c", va_arg(args, int));
+}
+/**
+ * print_int - prints an integer
+ * @args: the list of arguments
+ */
+
+void print_int(va_list args)
+{
+	 printf("%d", va_arg(args, int));
+}
+/**
+ * print_float - prints a float
+ * @args: the list of arguments
+ */
+
+void print_float(va_list args)
+{
+	 printf("%f", va_arg(args, double));
+}
+/**
+ * print_string - prints a string
+ * @args: the list of arguments
+ *
+ * Return: nothing
+ */
+
+void print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+		if (str == NULL)
+		{
+			printf("(nil)");
+			return;
+		}
+	printf("%s", str);
+}
+/**
+ * print_all - prints anything
+ * @format: a list of types of arguments passed to the function
+ *
+ * Return: nothing
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	unsigned int i = 0;
+	unsigned int j;
+	const char *separator = "";
+
+	print_t p[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}};
+
+	va_start(args, format);
+
+		while (format && format[i])
+		{
+			j = 0;
+			while (p[j].c != NULL)
+			{
+			if (format[i] == p[j].c[0])
+			{
+				printf("%s", separator);
+				p[j].f(args);
+				separator = ", ";
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	va_end(args);
+	printf("\n");
+}

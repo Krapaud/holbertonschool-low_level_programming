4f60e5940cd4de6b361d4108f032f36d417e633f
Krapaud <mur.mickael@gmail.com>
2025-07-16 12:43:34 +0200

    Fix formatting by adding a blank line before the function documentation in sum_them_all

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,12 +1,12 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
+
 /**
  * sum_them_all - returns the sum of all its parameters
  * @n: number of parameters
  * @...: variable number of parameters to sum
  * Return: sum of all parameters, or 0 if n is 0
  */
-
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;

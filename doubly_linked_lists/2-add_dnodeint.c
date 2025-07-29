c68195cbee62179f979008b97ef67f7e69f83439
Krapaud <mur.mickael@gmail.com>
2025-07-29 10:43:18 +0200

    Fix memory handling in add_dnodeint to prevent freeing uninitialized pointer

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -18,8 +18,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 		new_node = malloc(sizeof(dlistint_t));
 		if (new_node == NULL)
+		{
+			free(new_node);
 			return (NULL);
-
+		}
 		new_node->next = *head;
 		*head = new_node;
 

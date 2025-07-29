69daca58bfea315586723f926e54201b2915058f
Krapaud <mur.mickael@gmail.com>
2025-07-29 14:05:25 +0200

    Refactor add_dnodeint function to improve null pointer checks and memory handling

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -12,23 +12,21 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	if (head != NULL)
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
 	{
-		dlistint_t *new_node;
-
-		new_node = malloc(sizeof(dlistint_t));
-		if (new_node == NULL)
-		{
-			free(new_node);
-			return (NULL);
-		}
-		new_node->next = *head;
-		*head = new_node;
-
-		new_node->n = n;
-		new_node->prev = NULL;
-
-		return (new_node);
+		return (NULL);
 	}
-	return (NULL);
+	if (*head != NULL)
+	{
+		(*head)->prev = new_node;
+	}
+	new_node->next = *head;
+	*head = new_node;
+	new_node->n = n;
+	new_node->prev = NULL;
+
+	return (new_node);
 }

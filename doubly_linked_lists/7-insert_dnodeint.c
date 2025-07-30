c5b2ecb5499c887b65e15d72a5c0bf1b101e7665
Krapaud <mur.mickael@gmail.com>
2025-07-30 15:10:27 +0200

    Refactor insert_dnodeint_at_index to simplify logic and improve readability

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,7 +1,6 @@
 #include "lists.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
+
 /**
  * insert_dnodeint_at_index - inserts a new node at a given position
  * @h: double pointer to the head of the list
@@ -10,44 +9,35 @@
  *
  * Return: address of the new node, or NULL if it failed
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node, *temp;
+	dlistint_t *new_node, *temp = *h;
 	unsigned int i;
 
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	for (i = 0; temp != NULL && i < idx - 1; i++)
+		temp = temp->next;
+
+	if (temp == NULL)
+		return (NULL);
+
+	if (temp->next == NULL && i == idx - 1)
+		return (add_dnodeint_end(h, n));
+
 	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
 
 	new_node->n = n;
-	new_node->next = NULL;
-	new_node->prev = NULL;
+	new_node->next = temp->next;
+	new_node->prev = temp;
 
-	if (idx == 0)
-	new_node = add_dnodeint(h, n);
+	if (temp->next != NULL)
+		temp->next->prev = new_node;
 
-	if (idx == 1)
-	{
-		new_node = add_dnodeint(h, n);
+	temp->next = new_node;
 
-		(*h)->prev = new_node;
-	}
-	else
-	{
-		temp = *h;
-		for (i = 0; i < idx - 1; i++)
-		{
-			if (temp != NULL)
-			temp = temp->next;
-		}
-	}
-	if (temp != NULL)
-	{
-		new_node->next = temp->next;
-		new_node->prev = temp;
-		temp->next = new_node;
-	}
-	if (temp->next == NULL)
-		new_node = add_dnodeint_end(h, n);
 	return (new_node);
 }
-

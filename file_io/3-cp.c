7661a2f76a048bebf84c9851ba18a6116df152f1
Krapaud <mur.mickael@gmail.com>
2025-08-01 13:54:04 +0200

    Refactor file copy implementation to use low-level I/O functions; improve error handling and usage messages.

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,65 +1,83 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-/**
- * cpy - copies the contents of one file to another
- * @src: source file pointer
- * @dest: destination file pointer
- */
-void cpy(FILE *src, FILE *dest)
-{
-	char buf[BUFSIZ];
-	size_t n;
+#include <unistd.h>
 
-	while ((n = fread(buf, 1, sizeof(buf), src)) > 0)
-	{
-		if (fwrite(buf, 1, n, dest) != n)
-		{
-			fprintf(stderr, "write error\n");
-			exit(4);
-		}
-	}
+#define BUFFER_SIZE 1024
+#define PERMISSIONS 0664
 
-	if (ferror(src))
-	{
-		fprintf(stderr, "read error\n");
-		exit(5);
-	}
-}
 /**
- * * main - copies a file
- * * @argc: argument count
- * * @argv: argument vector
- * * Return: 0 on success, non-zero on error
+ * main - copies the content of a file to another file
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 on success, or exit with specific codes on error
  */
-int main(int argc, char **argv)
+int main (int argc, char *argv[])
 {
-	FILE *src, *dest;
+	int fd_from, fd_to;
+	ssize_t bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
 	{
-		fprintf(stderr, "usage: cpy src dest\n");
-		return (1);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
 
-	src = fopen(argv[1], "rb");
-
-	if (src == NULL)
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
 	{
-		fprintf(stderr, "cannot open %s for reading\n", argv[1]);
-		return (2);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
 
-	dest = fopen(argv[2], "wb");
-	if (dest == NULL)
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, PERMISSIONS);
+	if (fd_to == -1)
 	{
-		fprintf(stderr, "cannot open %s for writing\n", argv[2]);
-		fclose(src);
-		return (3);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		if (close(fd_from) == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+			exit(100);
+		}
+		exit(99);
 	}
-	cpy(src, dest);
-	fclose(src);
-	fclose(dest);
 
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write (fd_to, buffer, bytes_read);
+		if (bytes_written == -1 || bytes_written != bytes_read)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			if (close(fd_from) == -1)
+			{
+				dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+				exit(100);
+			}
+			if (close(fd_to) == -1)
+			{
+				dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+				exit(100);
+			}
+			exit(99);
+		}
+	}
+
+	if (close(fd_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		if (close(fd_to) == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		}
+		exit(100);
+	}
+
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
 	return (0);
 }
